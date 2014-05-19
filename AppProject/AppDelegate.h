8de26154ffd43ef2ad2ec99f1021f0acd0be70af
Daniel Rodosky <djrodosky@gmail.com>
2014-05-19 11:41:08 -0600

    Initial Commit

---
+++ AppProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppProject
+//
+//  Created by Daniel Rodosky on 5/19/14.
+//  Copyright (c) 2014 Dan Rodosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
