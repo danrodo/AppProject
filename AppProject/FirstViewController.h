8cdda0c5087a8d4121f5df7b685751af44d442ed
Daniel Rodosky <djrodosky@gmail.com>
2014-05-19 11:44:32 -0600

    initial commit
    initial commit

---
+++ AppProject/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  AppProject
+//
+//  Created by Daniel Rodosky on 5/19/14.
+//  Copyright (c) 2014 Dan Rodosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ AppProject/FirstViewController.m
@@ -0,0 +1,39 @@
+//
+//  FirstViewController.m
+//  AppProject
+//
+//  Created by Daniel Rodosky on 5/19/14.
+//  Copyright (c) 2014 Dan Rodosky. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+ // Dispose of any resources that can be recreated.
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
